8f2d9df169f3627e2841cfadd5c706cd1ab5a1c3
Dejan Sandic <20561859+DejanSandic@users.noreply.github.com>
2018-02-18 16:17:24 +0100

    New direction

--- index.js
+++ index.js
@@ -1,62 +1,55 @@
+//"use strict";
 const Fetch = (url, obj) => {
-    // Return the new promise
+    // Return a new promise.
     return new Promise((resolve, reject) => {
         // Check is the url argument present.
         if (!url) throw new Error("TypeError: Failed to execute 'fetch' on 'Window': " +
-            "1 argument required, but only 0 present.");
+            "1 argument required, but 0 present.");
         
         // Check is the url argument a string.
         if (typeof(url) !== 'string') throw new Error("TypeError: Failed to execute 'fetch' on 'Window': " +
-            "First argument is expected to be a string representing the route");
+            "The First argument is expected to be a string representing the route.");
 
         // If route for passed url is not defined, throw an error.
         if (!Fetch.__routes__[url]) {            
             throw new Error("TypeError: Failed to execute 'fetch' on 'Window': Failed to parse URL from " + url);
         } else {
-            // If request object (obj) doesn't have method specified,
-            // set GET as default method.
-            let responseBody, method = 'GET';
-            
-            // If request object (obj) havs method specified,
-            // transform it to upper case to avoid errors (get, GET, GeT).
+            // Store route in a variable
+            let route = Fetch.__routes__[url];
+
+            // If request object (obj) has 'method' specified, transform it to uppercase
+            // to avoid errors (post, POST, PoSt) and pass it to method variable.
+            // Otherwise set GET as default HTTP method.
+            let method = 'GET';
             if (obj && obj.method) method = obj.method.toUpperCase();
 
-            // Get the value from the method and store it in the responseBody variable.
-            responseBody = Fetch.__routes__[url].methods[method];
-
-            // Check is object with expected properties specified for current HTTP method,
-            // if not set it to empty object.
-            let expect = Fetch.__routes__[url].expect[method] || {};
-
-            // Set the response variable to return value of Fetch.Response factory function
-            let response = Fetch.Response(responseBody, expect);
-
-            // Take the value of the wait parameter from the obj and use it to set timeout,
-            // after that resolve the promise with the response variable.
+            // 1. Resolve the promise with the response object of selected method.
+            // 2. Pass wait property of selected method as timeout
             setTimeout(() => {
-                resolve(response);
-            }, Fetch.__routes__[url].wait);
+                resolve(route[method].response);
+            }, route[method].wait);
         }
     });
 }
 
-
 // Set the default timeout to 2000 ms.
-Fetch.timeout = 20000;
-
+Fetch.__timeout__ = 5000;
 
 // Method for changing the default timeout.
 Fetch.setTimeout = (timeout) => {
-    Fetch.timeout = timeout;
+    Fetch.__timeout__ = timeout;
 }
 
+// Method for returning current timeout.
+Fetch.getTimeout = () => {
+    return Fetch.__timeout__;
+}
 
 // Set the routes object to an empty object.
 Fetch.__routes__ = {
 }
 
-
-// Method for changing routes object to pre-defined routs object.
+// Method for changing routes object to pre-defined object.
 Fetch.setRoutes = (routes) => {
     // Throw an error if routes argument is not an object.
     if (!routes || typeof(routes) !== 'object') {
@@ -65,17 +58,23 @@ Fetch.setRoutes = (routes) => {
     Fetch.__routes__ = routes;
 }
 
-
 // Return a copy of the routes object.
+// Object.assign is used because NodeJS natively doens't support 
+// object spread syntax {...Fetch.__routes__}.
 Fetch.getRoutes = () => {
-    return {...Fetch.__routes__};
+    return Object.assign({}, Fetch.__routes__);
 }
 
+// Function for adding Fetch.__mehtods__ prototype to passed object
+// by assigning obj to empty object which has Fetch.__Methods__ on it's prototype.
+const addFetchMethods = (obj) => {
+    return Object.assign(Object.create(Fetch.__Methods__) , obj)
+}
 
-// Method for creating new routes.
+// Method for adding new routes
 Fetch.addRoute = (route, obj) => {
     // Throw an error if the route already exists.
-    if (Fetch.__routes__[route]) throw new Error('Route already exists, if you want to remove it use Fetch.removeRoute.');
+    if (Fetch.__routes__[route]) throw new Error('Route ' + route + ' already exists, if you want to remove it use Fetch.removeRoute method.');
 
     // Throw an error if route is not a string and obj is not an object.
     if (!route || typeof(route) !== 'string' || !obj || typeof(obj) !== 'object') {
@@ -83,22 +82,62 @@ Fetch.addRoute = (route, obj) => {
             + 'representing the route, and second argument to be a response object for that route.');
     }
 
-    // Throw an error if obj argument does not have .response property.
-    if (!obj.response) throw new Error ('Response object must have a .response property.');
+    // Create empty object which represents the new route.
+    let newRoute = {};
 
-    // Throw an error if wait propety is not a number.
-    if (obj.wait && typeof(obj.wait) !== 'number') throw new Error("The wait property needs to be a number representing " +
-        "the delay of the response");
+    // For each method (GET, POST, PUT, etc..) on the obj do,
+    // if that method is not empty object...
+    for (let method in obj) {
+        if (Object.keys(obj).length !== 0) {
 
-    // Check does the wait property exceed the default timeout.
-    if (obj.wait > Fetch.timeout) throw new Error("The wait  property exceeds the default timeout limit. " +
-        "To change the default timeout limit use Fetch.setTimeout method");
+            // Throw an error if expect propety is not an object.
+            if (obj[method].expect && typeof(obj[method].expect) !== 'object') throw new Error("The expect property needs to be an object.");
 
-    // If timeout is not set, set it to 0 ms.
-    obj.wait = obj.wait || 0;
+            // Throw an error if wait propety is not a number.
+            if (obj[method].wait && typeof(obj[method].wait) !== 'number') throw new Error("The wait property needs to be a number representing " +
+                "the delay of the response.");
 
-    // Using Route contructor, create new route on the routes object
-    Fetch.__routes__[route] = new Fetch.Route(obj);
+            // Check does the wait property exceed the default timeout.
+            if (obj[method].wait > Fetch.__timeout__) throw new Error("The wait  property exceedes the default timeout limit. " +
+                "To change the default timeout limit use Fetch.setTimeout method.");
+
+            // 1. If expect on obj is not defined, set it to an empty object.
+            // 2. If wait on obj is not defined, set it to 0 by default.
+            // 3. If response on obj is not defined, set it to null.
+            // 4. Set url to route argument passed to the Fetch.addRoute method 
+            let expect = obj[method].expect || {};
+            let wait = obj[method].wait || 0;
+            let response =  obj[method].response || null;
+            let url = route;
+
+            // Create responseObj.
+            let responseObject = {
+                // Use addFetchMethods function to merge response property
+                // with another object which has Fetch__Methods__ on its prototype.
+                // This way our response property (object) will have methods 
+                // like .json() on it's prototype.
+                // Use Object.assign method to spread expect object and add response and url properties on it.
+                // Object.assign is used because NodeJS natively doens't support 
+                // object spread syntax { response, ...expect, url }.
+                response: addFetchMethods(
+                    Object.assign({},
+                        expect,
+                    {
+                        response,
+                        url
+                    })
+                ),
+                wait
+            }
+
+            // Transform method name to uppercase (get = GET) and
+            // create that method on newRoute object.
+            newRoute[method.toUpperCase()] = responseObject;
+        }
+
+        // Add new route to Fetch.__routes__ object
+        Fetch.__routes__[route] = newRoute;
+    }
 }
 
 // Method for removing routes.
@@ -116,19 +155,11 @@ Fetch.removeRoute = (route) => {
     delete Fetch.__routes__[route];
 }
 
-
 // Use Fetch API by replacing the fetch API with it
 Fetch.use = (routes) => {
-    // Set the globalObj to window object in browser environment, or
-    // to global object in Node environment.
-    let globalObject;
-    try {
-        globalObject = window;
-    } catch (err) {}
-
-    try {
-        globalObject = global;
-    } catch (err) {}
+    // Set the globalObj to window object in the browser environment, or
+    // to global object in the Node environment.
+    let globalObject = window || global;
 
     // If routes argument is provided and its value is anobject, use it.
     // If routes argument is provided and its value is not an object, throw an error.
@@ -142,15 +173,9 @@ Fetch.use = (routes) => {
     globalObject.fetch = Fetch;
 }
 
-
-// *************************************
-// RESPONSE METHODS
-// *************************************
-
-// Object for methods on Fetch.Responcse response.
+// Object containing methods on the Fetch response object prototype.
 Fetch.__Methods__ = {
-    // Initial json method which just returns the response
-    // within the resolved promise .
+    // Initial json method which just returns the response within the resolved promise.
     json: function () {
         let self = this;
         return new Promise ((resolve, redject) => {
@@ -159,7 +184,7 @@ Fetch.__Methods__ = {
     }
 }
 
-// Method for adding methods on the Fetch response object prototype
+// Method for adding methods on the Fetch response object prototype.
 Fetch.addMethod = (name, func) => {
     // Throw an error if name is not a string and func is not a function.
     if (!name || typeof(name) !== 'string' || !func || console.log({}.toString.call(func) !== '[object Function]')) {
@@ -167,59 +192,6 @@ Fetch.addMethod = (name, func) => {
             'the name of the method, and second argument to be anonimous funciton.');
     }
     Fetch.__Methods__[name] = func;
-}
-
-
-// *************************************
-// CONSTURUCTORS
-// *************************************
-
-// Route constructor.
-Fetch.Route = function (obj) {
-    // Create temporary, empty methods object.
-    let methods = {};
-
-    // If wait is not specified set 0 as default.
-    this.wait = obj.wait;
-
-    // Set the expect object to empty object
-    this.expect = {};
-
-    // If obj.response is not an object, set the default http method to GET
-    // and assign obj.response as its value.
-    if (typeof(obj.response) !== 'object') {
-        methods.GET = obj.response;
-
-    // If obj.response is an object, for each of its defined HTTP metods, 
-    // create corresponding methods with responses on the methods object.
-    } else {
-        for (let prop in obj.response) {
-            methods[prop.toUpperCase()] = obj.response[prop];
-        }
-    }
-    this.methods = methods;
-
-    // If obj has expected an object with expected properties, add those
-    // properties to the expect object of the new route
-    if (obj.expect) {
-        for (let prop in obj.expect) {
-            this.expect[prop.toUpperCase()] = obj.expect[prop];
-        }
-    }
-}
-
-// Response factory function.
-Fetch.Response = (response, expect) => {
-    // Generate new object with Fetch methods on it's prototype
-    let obj = Object.create(Fetch.__Methods__);
-    
-    obj.response = response;
-
-    // For each property in expect, add that property to the response object
-    for (let prop in expect) {
-        obj[prop] = expect[prop];
-    }
-    return obj;
 }
 
 try {
